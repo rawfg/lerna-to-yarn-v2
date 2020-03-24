e5c8b799792ea76d5ca01952fc6da64fc3dcd1b9
Rafal Gorczynski <rafal.gorczynski@gmail.com>
2020-03-24 07:03:12 +0100

    initial commit

---
+++ packages/app/src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { ModuleA } from "module-a";
+import { ModuleB } from "module-b";
+
+function App() {
+  return (
+    <div>
+      <ModuleA />
+      <ModuleB />
+    </div>
+  );
+}
+
+export default App;
